71d1cc6c091e955e6281f543aec9157bf3ddb743
kvnx <35074883+kvnx@users.noreply.github.com>
2020-07-15 21:38:28 -0500

    idk

--- src/renderTerminal.js
+++ src/renderTerminal.js
@@ -90,9 +90,7 @@ const renderTerminal = (info = {}, options = { languages: [] }) => {
       ${hide_border ? "" : border}
       
       <text x="25" y="35" class="text">${name}@github</text>
-      <svg x="0" y="45">
-        ${infoItems.toString().replace(/\,/gm, "")}
-      </svg>
+      
     </svg>
   `;
 
